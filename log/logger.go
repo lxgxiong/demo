af7e8266d39296730ffae31117b12aac7a1d9b04
leixiong <lxgxiong@vip.qq.com>
2017-05-23 13:16:24 +0800

    update logger

--- log/logger.go
+++ log/logger.go
@@ -1,1 +1,39 @@
 package log
+
+
+import (
+	"fmt"
+	"os"
+	"github.com/kardianos/osext"
+	"path/filepath"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"github.com/go-kit/kit/log"
+)
+
+var Logger log.Logger
+
+func init() {
+	folderPath, err := osext.ExecutableFolder()
+	if err != nil {
+		fmt.Println("error when initializing logger:", err)
+		os.Exit(1)
+	}
+	base := filepath.Base(folderPath)
+
+	lumberOut := &lumberjack.Logger{
+		Filename:   fmt.Sprint("./logs/", base, ".log"),
+		MaxSize:    100, // megabytes
+		MaxBackups: 10,
+		MaxAge:     30, //days
+	}
+
+	Logger = log.NewLogfmtLogger(lumberOut)
+	Logger = log.With(Logger, "ts", log.DefaultTimestamp)
+}
+
+func Log(keyValues... interface{})  {
+	err :=Logger.Log(keyValues...)
+	if err!=nil {
+		fmt.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,11 +1,29 @@
-package demo
+package main
 
 import (
+	"demo/config"
+	"github.com/BurntSushi/toml"
+	"demo/log"
+	"flag"
+	"demo/user"
 	_ "github.com/go-sql-driver/mysql"
+	"fmt"
 )
 
-
+var filePath = flag.String("config", "app.toml", "configuration file path")
 
 func main() {
+	flag.Parse()
+
+	//loading configurations
+	var conf config.ApplicationConfig
+	if _, err := toml.DecodeFile(*filePath, &conf); err != nil {
+		log.Log("err", err.Error())
+		return
+	}
+
+	var repo = user.NewRepository(conf.Mysql)
+	var svc = user.NewService(repo)
+	fmt.Println(svc.Register("test1","test"))
 
 }
